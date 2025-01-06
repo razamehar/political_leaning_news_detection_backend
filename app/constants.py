6c89c818916903e9cbd483f3d66d45bd7857b0d0
Pujan Thapa <iampujan@outlook.com>
2025-01-06 21:34:37 +0100

    constant file updated for news source

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+NEWS_SOURCES = {
+    "Wired": "wired",
+    "ABC News": "abc-news",
+    "Business Insider": "business-insider",
+    "Fox News": "fox-news",
+    "The Verge": "the-verge",
+    "Bleacher Report": "bleacher-report",
+    "Bloomberg": "bloomberg",
+    "CNN": "cnn",
+    "BBC News": "bbc-news",
+}
